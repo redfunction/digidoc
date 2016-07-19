a885902bea360d1c402a9b14fbd0dfd515681b43
Margus Laak <margus.laak@redfunction.ee>
2016-07-20 00:50:31 +0300

    add better documentation to signing.php

--- examples/idcard/signing.php
+++ examples/idcard/signing.php
@@ -1,5 +1,21 @@
+<html><body>
+<pre>
+    This example uses hwcrypto - https://github.com/open-eid/hwcrypto.js
+    The sequence in all of this:
+    1. Start a session
+        1.1 if we want to add a signature to existing bdoc, then pass the base64 of bdoc to startSession
+        1.2 if we want to create a new bdoc, then use addFile
+        1.3 do not use 1.2 and 1.1 at the same time - you will probably get an error
+    2. ask a certificate from Id Card with hwcrypto in browser
+    3. send it to server with ajax and use prepareSignature
+    4. use prepare signature output for hwcrypto input and ask for signature from Id Card with hwcrypto in browser
+    5. send it to server with ajax and use finalizeSignature
+    6. downloadContainer on server side
+</pre>
 <?php
 
+// nuke the cache just in case
+// there have been cases during development, when invalid wsdl was saved
 ini_set('soap.wsdl_cache_enabled', '0');
 ini_set('soap.wsdl_cache_ttl', '0');
 
@@ -15,6 +31,11 @@ $digiDocService = new DigiDoc(DigiDoc::URL_TEST);
 /** @var FileSignerInterface $signer */
 $signer = $digiDocService->getService(FileSignerInterface::class);
 
+/**
+ * Here starts the ajax part.
+ *
+ * 1. Ask certificate from browser and store it in SK
+ */
 if(!empty($_POST["certificate"])) {
     $signer->setSessionCode($_POST["session"]);
     $data = $prepareSignatureResponse = $signer->prepareSignature($_POST['certificate']);
@@ -23,6 +44,9 @@ if(!empty($_POST["certificate"])) {
     exit;
 }
 
+/**
+ * 2. Finalize the signature from browser and store it in SK
+ */
 if(!empty($_POST["signature"])) {
     $signer->setSessionCode($_POST["session"]);
     $data = $prepareSignatureResponse = $signer->finalizeSignature($_POST['signature_id'], $_POST['signature']);
@@ -36,28 +60,30 @@ if(!empty($_POST["signature"])) {
     exit;
 }
 
-
-print '<html><body>';
-
 $bdocContent = '';
 if (file_exists(__DIR__ . '/example.document.bdoc')) {
+    print 'We add new signature to existing BDOC';
     $bdocContent = base64_encode(file_get_contents(__DIR__ . '/example.document.bdoc'));
 } else {
+    print 'We create a new BDOC and add a signature to it';
     $fileContent = base64_encode(file_get_contents(__DIR__ . '/example.document.pdf'));
 }
 
+// the session has to be started, no matter what.
+// if we are adding a new signature, then the base64 of bdoc must be passed here as well
 $signer->startSession($bdocContent);
 
-printf("Uploading the file...\n");
-
 if (empty($bdocContent) && !empty($fileContent)) {
+    // if we did not add new signature to existing bdoc, then we need to add a data file which we are going to sign
     $signer->addFile('documet.pdf', 'application/pdf', $fileContent);
 }
 
-printf("Signing...\n");
-
-print "<script src=\"hwcrypto.js\"></script>";
 ?>
+<p>
+This is the frontend part of the signing
+</p>
+
+<script src="hwcrypto.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
 <script>
 
@@ -66,21 +92,6 @@ print "<script src=\"hwcrypto.js\"></script>";
 
 function log_text(msg) {
     console.log(msg);
-}
-
-function sign() {
-
-    window.hwcrypto.getCertificate({lang: lang}).then(function(response) {
-      var cert = response;
-      log_text("Using certificate:\n" + hexToPem(response.hex));
-      window.hwcrypto.sign(cert, {hex: hash}, {lang: lang}).then(function(response) {
-        log_text("Generated signature:\n" + response.hex.match(/.{1,64}/g).join("\n"));
-      }, function(err) {
-        log_text("sign() failed: " + err);
-      });
-    }, function(err) {
-      log_text("getCertificate() failed: " + err);
-    });
 }
 
 function onSign(response) {
@@ -105,6 +116,6 @@ function prepareSignature(event) {
 }
 </script>
 
-    <button type="button" onclick="prepareSignature(event)">Sign</button>
+    <button type="button" onclick="prepareSignature(event)">Click here, to start signing</button>
 
 </body></html>
